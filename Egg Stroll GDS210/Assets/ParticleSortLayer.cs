79843b28973aca167d5ce0d7af9251c90ab13cd3
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-10-08 16:23:38 +0800

    Egg Jumping
    Jump

---
+++ Egg Stroll GDS210/Assets/ParticleSortLayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSortLayer : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "Particles";
+    }
+}
