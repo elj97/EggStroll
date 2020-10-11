424a71e7f9283d81ab66c70ab60c54b56e31f037
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-10-11 20:41:36 +0800

    Added Coins, Scoring and End Screen for Winning
    Used mock sprites in place, since making it in my branch I have certain codes on their own sprite but most will be merged into others (most will be combined with the PlayerController script).
    
    Basically I added a collectible coin that increases the number in the top left. I also added that when you pass an invidible "Finish Line" a trigger will happen which, when I have access to the main branch, will slow the egg down until it reaches the nest, once colliding with the nest the end win screen will appear, currently you can only retry the current level but I plan to make it show your score and allow you to go to the next level. All mock sprites and ui though

---
+++ Egg Stroll GDS210/Assets/PassFinishLine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassFinishLine : MonoBehaviour
+{
+
+    public GameObject finishLine;
+    private bool hasFinished = false;
+    public GameObject nest;
+    public GameObject endScreen;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (hasFinished == false)
+        {
+            //Jumping and Player interaction of the egg
+        }
+
+        if (hasFinished == true)
+        {
+            //Change speed of sprite animation
+            //Change speed of screen movement
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject == finishLine)
+        {
+            hasFinished = true;
+        }
+        if (collider.gameObject == nest)
+        {
+            endScreen.SetActive(true);
+        }
+    }
+
+    
+}
