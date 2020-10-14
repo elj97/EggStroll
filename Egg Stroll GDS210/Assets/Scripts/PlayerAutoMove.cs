0d9edcdf921fc0cd9514f7831c0dba921fb1ed96
elj97 <1020194@student.sae.edu.au>
2020-10-14 17:42:59 +0800

    restartdone

--- Egg Stroll GDS210/Assets/Scripts/PlayerAutoMove.cs
+++ Egg Stroll GDS210/Assets/Scripts/PlayerAutoMove.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerAutoMove : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
---
+++ Egg Stroll GDS210/Assets/Scripts/Restart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    void OnCollisionEnter2D (Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+
+            RestartLevel();
+        }
+    }
+
+    /* void Start()
+     {
+         if (ClassName.playRestartSound)
+         {
+             ClassName.playRestartSound = false;
+             audioSource.Play();
+         }
+     }*/
+
+    public void RestartLevel()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
