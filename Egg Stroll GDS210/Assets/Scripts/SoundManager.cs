5f017b8d5e6a85cd868ad55b2a78d503ad36bc7b
elj97 <1020194@student.sae.edu.au>
2020-10-14 18:55:44 +0800

    end of class

--- Egg Stroll GDS210/Assets/Scripts/SoundManager.cs
+++ Egg Stroll GDS210/Assets/Scripts/SoundManager.cs
@@ -1,22 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
-
     public static AudioClip playerCrackSound, playerJumpSound;
     private static AudioSource audioSource;
+  
 
     void Start()
     {
-
         playerCrackSound = Resources.Load<AudioClip>("eggCrack");
         playerJumpSound = Resources.Load<AudioClip>("jump");
 
         audioSource = GetComponent<AudioSource>();
     }
+    /*
+    void Awake()
+    {
+        Debug.Log(ApplicationActivator.loadedSceneName);
 
+       // DontDestroyOnLoad(this.gameObject);
+
+        soundtrack[0] = (AudioClip)ResourceScope.Load("MenuSoundtrack");
+
+         
+        if(ApplicationActivator.loadedSceneName == "MainMenu")
+        {
+            audioSource.clip[0];
+            audioSource.Play();
+        }
+         
+
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+    */
     void Update()
     {
 
