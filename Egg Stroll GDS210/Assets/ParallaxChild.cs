6eab07afc5871bdc323c579bd395d08e6ce13854
elj97 <1020194@student.sae.edu.au>
2020-10-15 18:40:23 +0800

    Parallax is working
    :)

---
+++ Egg Stroll GDS210/Assets/ParallaxChild.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxChild : MonoBehaviour
+{
+    [SerializeField] private bool isRight;
+
+    void OnValidate()
+    {
+        var length = GetComponent<SpriteRenderer>().bounds.size.x;
+        if (!isRight)
+        {
+            length *= -1;
+        }
+        transform.localPosition = new Vector3(length, 0, 0);
+    }
+}
