949d55ad2ef02e1bd1404925b1508aa4ac88be81
Gemma Ramage <1008468@student.sae.edu.au>
2020-10-15 20:29:22 +0800

    Version with UI

---
+++ Egg Stroll GDS210/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseMenu = null;
+
+    void Awake()
+    {
+        gamePaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused == false)
+            {
+                PauseGame();
+            }
+            else
+            {
+                UnPauseGame();
+            }
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (gamePaused == false)
+        {
+            PauseGame();
+        }
+        else
+        {
+            UnPauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0.000001f;
+        gamePaused = true;
+    }
+
+    public void UnPauseGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    public void Settings()
+    {
+        Debug.Log("There ain't no settings!");
+    }
+
+    public void LevelSelect()
+    {
+        Debug.Log("There's only one level!");
+    }
+}
---
+++ Egg Stroll GDS210/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public void Scene1()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void Scene2()
+    {
+        SceneManager.LoadScene("EggStroll");
+    }
+}
