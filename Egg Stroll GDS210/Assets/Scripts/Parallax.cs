322d7fdd854638016edd71315d8c55ec7161725f
elj97 <1020194@student.sae.edu.au>
2020-10-15 20:05:04 +0800

    Level is Complete
    Basically (Besides pause)

---
+++ Egg Stroll GDS210/Assets/Scripts/Parallax.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos, ypos;
+    public GameObject cam;
+    public float parallaxEffectx;
+    public float parallaxEffecty;
+    public float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        ypos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffectx));
+        float dist = (cam.transform.position.x * parallaxEffectx);
+        float ydist = (cam.transform.position.y * parallaxEffecty);
+
+        transform.position = new Vector3(startpos + dist, ypos + ydist, transform.position.z);
+        if (temp > startpos + (length - offset))
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - (length - offset))
+        {
+            startpos -= length;
+        }
+    }
+}
--- Egg Stroll GDS210/Assets/Scripts/ScoreManager.cs
+++ Egg Stroll GDS210/Assets/Scripts/ScoreManager.cs
@@ -9,6 +9,7 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
     public TextMeshProUGUI text;
     int score;
+    public TextMeshProUGUI finalScoreText;
 
     void Start()
     {
@@ -22,5 +23,13 @@ public class ScoreManager : MonoBehaviour
     {
         score += coinValue;
         text.text = "Score " + score.ToString();
+    }
+
+    private void Update()
+    {
+        if (finalScoreText !== null)
+        {
+            finalScoreText.text = score.ToString();
+        }
     }
 }
